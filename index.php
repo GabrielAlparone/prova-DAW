043486aea777dd4febe19fff20af393b761007e0
GabrielAlparone <125314218+GabrielAlparone@users.noreply.github.com>
2023-10-03 11:20:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,87 +14,72 @@
         $codigo = $_POST['codigo'];
         $valor = $_POST['valor'];
 
-<?php function inserirCarrinho ()
-  {
-          if(!file_exists("arquivo.txt")){
-                $arqCompra = fopen("arquivo.txt", "w");
-                fwrite($nome, $codigo, $valor);
-            }
-
-             else {
-                $arqCompra = fopen("arquivo.txt", "a");
-              fwrite($nome, $codigo, $valor);
-            fclose($arqCompra);
-            }
-  }?>
-
   ?> 
-         <form method="post" action="">
-        <label for="nome"></label><br>
-        <label for="codigo">:</label><br>
+         <form>
+        <label for="nome"></label>
+        <label for="codigo">:</label>
         <label for="valor">:</label><br>
-  
      </form>
-     <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Shampoo Cachos 250 Ml"></label><br>
-        <label for="21">:</label><br>
-        <label for="22,99">:</label><br>
+     <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Shampoo Cachos 250 Ml"></label>
+        <label for="21">:</label>
+        <label for="R$22,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Cachos Exuberantes)  - Condicionador 250 Ml"></label><br>
-        <label for="22">:</label><br>
-        <label for="23,99">:</label><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes)  - Condicionador 250 Ml"></label>
+        <label for="22">:</label>
+        <label for="R$23,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Condicionador 250 Ml"></label><br>
-        <label for="23">:</label><br>
-        <label for="24,99">:</label><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Condicionador 250 Ml"></label>
+        <label for="23">:</label>
+        <label for="R$24,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Liso Perfeito)  - Shampoo Alisador 250 Ml"></label><br>
-        <label for="25">:</label><br>
-        <label for="25,99">:</label><br>
-        <input type="button" id="submit" name="inserir" required><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Liso Perfeito)  - Shampoo Alisador 250 Ml"></label>
+        <label for="25">:</label>
+        <label for="R$25,99">:</label>
+        <input type="button" id="submit" name="inserir" required>
      </form>
-         <form method="post" action="">
+         <form method="post" action="inserircarrinho.php">
         <label for="Boticario Cuide-se Bem (Liso Perfeito) - Shampoo Alisador 250 Ml"></label><br>
-        <label for="26">:</label><br>
-        <label for="22,99">:</label><br>
+        <label for="26">:</label>
+        <label for="R$22,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Liso Perfeito) - Condicionador 250 Ml"></label><br>
-        <label for="27">:</label><br>
-        <label for="24,99">:</label><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Liso Perfeito) - Condicionador 250 Ml"></label>
+        <label for="27">:</label>
+        <label for="R$24,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
+         <form method="post" action="inserircarrinho.php">
         <label for="Boticario Cuide-se Bem (Restauracao Extraordinaria)  - Condicionador 250 Ml"></label><br>
         <label for="28">:</label><br>
-        <label for="24,99">:</label><br>
+        <label for="R$24,99">:</label><br>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Restauracao Extraordinaria) - Condicionador 250 Ml"></label><br>
-        <label for="29">:</label><br>
-        <label for="24,99">:</label><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Restauracao Extraordinaria) - Condicionador 250 Ml"></label>
+        <label for="29">:</label>
+        <label for="R$24,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
          </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label><br>
-        <label for="30">:</label><br>
-        <label for="24,99">:</label><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label>
+        <label for="30">:</label>
+        <label for="R$24,99">:</label>
         <input type="button" id="submit" name="inserir" required><br>
      </form>
-         <form method="post" action="">
-        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label><br>
-        <label for="31">:</label><br>
-        <label for="24,99">:</label><br>
-        <input type="button" id="submit" name="inserir" required><br>
+         <form method="post" action="inserircarrinho.php">
+        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label>
+        <label for="31">:</label>
+        <label for="R$24,99">:</label>
+        <input type= "button" id="submit" name="inserir" required><br>
      </form>
 
  
