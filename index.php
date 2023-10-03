bc5a31c6aeff8a01a403df11ce7814e7c6bbc62b
GabrielAlparone <125314218+GabrielAlparone@users.noreply.github.com>
2023-10-03 11:01:14 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>LOJAS ALPARONE!</title>
+  </head>
+  <body>
+    
+    <?php 
+        $nome = "";
+        $codigo = "";
+        $valor = "";
+
+        $nome = $_POST['nome'];
+        $codigo = $_POST['codigo'];
+        $valor = $_POST['valor'];
+      
+
+            
+
+
+  ?> 
+         <form method="post" action="">
+        <label for="nome"></label><br>
+        <label for="codigo">:</label><br>
+        <label for="valor">:</label><br>
+  
+     </form>
+     <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Shampoo Cachos 250 Ml"></label><br>
+        <label for="21">:</label><br>
+        <label for="R$22,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes)  - Condicionador 250 Ml"></label><br>
+        <label for="22">:</label><br>
+        <label for="R$23,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Cachos Exuberantes) - Condicionador 250 Ml"></label><br>
+        <label for="23">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Liso Perfeito)  - Shampoo Alisador 250 Ml"></label><br>
+        <label for="25">:</label><br>
+        <label for="R$25,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Liso Perfeito) - Shampoo Alisador 250 Ml"></label><br>
+        <label for="26">:</label><br>
+        <label for="R$22,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Liso Perfeito) - Condicionador 250 Ml"></label><br>
+        <label for="27">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Restauracao Extraordinaria)  - Condicionador 250 Ml"></label><br>
+        <label for="28">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Restauracao Extraordinaria) - Condicionador 250 Ml"></label><br>
+        <label for="29">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label><br>
+        <label for="30">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+         <form method="post" action="">
+        <label for="Boticario Cuide-se Bem (Hidratacao Imediata)  - Shampoo Hidratante 250 Ml"></label><br>
+        <label for="31">:</label><br>
+        <label for="R$24,99">:</label><br>
+        <input type="button" id="submit" name="inserir" required><br>
+     </form>
+
+ 
+  </body>
+</html>
